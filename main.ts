dfa01cbb359ea63f020d7807e2f5159fa84a7189
TheM1rk <74301709+TheM1rk@users.noreply.github.com>
2022-05-24 19:44:30 +0200

    tinkering

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
 let JY, JX, JYF, JXF,XZ:boolean,YZ:boolean,STOP:boolean;
 radio.setGroup(1)
 // to check if there is a connection
@@ -7,20 +16,24 @@ basic.forever(function () {
     // same thing for axis y
     YZ = false
     // read Y
-    STOP=false
-    //stop is used when no input is send, it prevents from
-    //spamming packages
+    STOP = false
+    // stop is used when no input is send, it prevents from
+    // spamming packages
     JY = pins.analogReadPin(AnalogPin.P2)
     // read X
     JX = pins.analogReadPin(AnalogPin.P1)
     // JX or JY stands for "joystic X/Y"
     if (JX >= 500 && JX <= 512) {
         XZ = true
+    } else {
+        XZ = false
     }
     // in case the joystic is in the middle the XZ(X-ZERO) becomes true
     // thus not doing anything
     if (JY >= 500 && JY <= 510) {
         YZ = true
+    } else {
+        YZ = false
     }
     if (JX >= 1018) {
         JX = 1023
@@ -31,22 +44,21 @@ basic.forever(function () {
     // this is used to have a static number, after "1018"
     // the value will result 1023.
     if (!(YZ)) {
-        radio.sendValue("movimentoX", JX)
-    }
-    if (!(XZ)) {
         radio.sendValue("movimentoY", JY)
     }
-    else if (XZ&&YZ)
-        STOP=true;
-    else
-        STOP = false
-
-    if(STOP)
-    {
-        radio.sendString("STOP");
-        STOP=false;
+    if (!(XZ)) {
+        radio.sendValue("movimentoX", JX)
     }
     
+    if (XZ && YZ) {
+        STOP = true
+    } else {
+        STOP = false
+    }
+    if (STOP) {
+        radio.sendString("STOP")
+        STOP = false
+    }
     if (radio.receivedPacket(RadioPacketProperty.SignalStrength) == 0) {
         basic.showLeds(`
             . # . # .
