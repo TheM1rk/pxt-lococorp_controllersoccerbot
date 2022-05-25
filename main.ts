25f438db803854541b1914912edbc2cc188bedae
TheM1rk <74301709+TheM1rk@users.noreply.github.com>
2022-05-25 20:34:38 +0200

    best code for the controller

--- main.ts
+++ main.ts
@@ -7,56 +7,36 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
 })
+let XZero = false
 let STOPX = false
 let STOPY = false
 let YZero = false
-let XZero = false
 let joystickY, joystickX, joystickYS, joystickXS;
 radio.setGroup(111)
+let movimento:Array<number>
 // to check if there is a connection
+// this is used to have a static number, after "1020"
+// the value will result 1023.
 basic.forever(function () {
     // read y
     joystickY = pins.analogReadPin(AnalogPin.P2)
     // read X
     joystickX = pins.analogReadPin(AnalogPin.P1)
-    if (joystickX >= 490 && joystickX <= 620) {
+    if (joystickX >= 490 && joystickX <= 530) {
         // its in the center of the joystick. so no input
-        XZero = true
-    } else {
-        XZero = false
+        joystickX = 500
     }
     // in case the joystic is in the middle the XZERO becomes true
     // thus not doing anything
-    if (joystickY >= 490 && joystickY <= 620) {
+    if (joystickY >= 490 && joystickY <= 530) {
         // is in the center of the joystick, so no input
-        YZero = true
-    } else {
-        YZero = false
+        joystickY = 500
     }
-    if (joystickX >= 1012) {
+    if (joystickX >= 1010) {
         joystickX = 1023
     }
-    if (joystickY >= 1012) {
+    if (joystickY >= 1009) {
         joystickY = 1023
     }
-    // this is used to have a static number, after "1020"
-    // the value will result 1023.
-    if (!(YZero)) {
-        radio.sendValue("m-Y", joystickY)
-    } else {
-        STOPY = true
-    }
-    if (!(XZero)) {
-        radio.sendValue("m-X", joystickX)
-    } else {
-        STOPX = true
-    }
-    // to reset the x value
-    if (STOPX) {
-        radio.sendString("STOPX")
-    }
-    // to stop the y value
-    if (STOPY) {
-        radio.sendString("STOPY")
-    }
+    radio.sendString("" + joystickX + " " + joystickY)
 })
